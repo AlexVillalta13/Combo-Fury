8c569cc7e921a458741c4e099626160b2ed465e7
alexvf93@gmail.com <>
2023-08-23 21:31:25 +0200

    - Top Bar in Main Menu visuals added

---
+++ Assets/Code/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera gameplayCamera;
+    [SerializeField] CinemachineVirtualCamera mainMenuCamera;
+
+    bool mainMenuEnabled = true;
+
+    public void switchPriority()
+    {
+        mainMenuEnabled = !mainMenuEnabled;
+        if (mainMenuEnabled == true)
+        {
+            mainMenuCamera.Priority = 1;
+            gameplayCamera.Priority = 0;
+        } else
+        {
+            mainMenuCamera.Priority = 0;
+            gameplayCamera.Priority = 1;
+        }
+    }
+}
